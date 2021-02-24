5b59b49df392322960ccae122b1cb4eec6653857
Michael Jolley <mike@sparcapp.io>
2021-02-24 13:59:46 -0600

    here we go again

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -7,9 +7,92 @@ export interface CredentialsObject {
 	privateKey?: string;
 }
 
+/* verify API */
+export interface VerifyError extends VonageApiError {
+	status: RequestResponseStatusCode | ControlResponseStatusCode | string;
+	error_text: string;
+	[key: string]: any;
+}
+
+export interface RequestObject {
+	brand: string;
+	number: string;
+	sender_id?: string;
+	country?: string;
+	code_length?: number;
+	lg?: string;
+	require_type?: string;
+	pin_expiry?: number;
+	next_event_wait?: number;
+	workflow_id?: number;
+}
+
+export interface RequestResponse {
+	request_id: string;
+	status: string;
+}
+
+export enum RequestResponseStatusCode {
+	Success = "0",
+	Throttled = "1",
+	MissingParameters = "2",
+	InvalidCredentials = "3",
+	InternalError = "4",
+	NotProcessed = "5",
+	BlackListedNumber = "6",
+	BlockedAccount = "7",
+	QuotaExceeded = "8",
+	ConcurrentVerificationNumber = "9",
+	TargetNetworkNotSupported = "10",
+	WrongVerificationCode = "11",
+	TooManyRequests = "12",
+	NoMoreEvents = "13",
+	NoRequestFound = "14",
+}
+
+export interface ControlObject {
+	request_id: string;
+	cmd: string;
+}
+
+export interface ControlResponse {
+	status: string;
+	command: string;
+}
+
+export enum ControlResponseStatusCode {
+	Success = "0",
+	CancelOrTriggerNextEvent = "19",
+}
+
+export interface CheckObject {
+	request_id: string;
+	code: string;
+	ip_address?: string;
+}
+
+export interface CheckResponse {
+	request_id: string;
+	event_id: string;
+	status: string;
+	price: string;
+	currency: string;
+}
+
+export class Verify {
+	constructor(credentials: CredentialsObject, options: { [key: string]: any });
+	request(request: RequestObject, callback: (err: VerifyError, data: RequestResponse) => void): void;
+	control(request: ControlObject, callback: (err: VerifyError, data: ControlResponse) => void): void;
+	check(request: CheckObject, callback: (err: VerifyError, data: CheckResponse) => void): void;
+	[key: string]: any;
+}
+
 export default class Vonage {
 	credentials: CredentialsObject;
 	options: { [key: string]: any };
 
-	constructor(credentials: CredentialsObject, options?: { [key: string]: any }): void;
+	constructor(credentials: CredentialsObject, options?: { [key: string]: any });
+
+	verify: Verify;
+
 }
